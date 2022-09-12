0df1dec20109bc805361c5ce7472a83bc8818545
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2022-09-12 09:21:37 +0530

    instructions at registration added

--- src/Screens/LandingPage.js
+++ src/Screens/LandingPage.js
@@ -1,25 +1,49 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Image from "../assets/wallpaper1.jpg";
 import Login from "../Components/Login";
+import Point from "../assets/points.png";
+import Cross from "../assets/cross.png";
 import { useNavigate } from "react-router-dom";
-import { ReactSession } from 'react-client-session';
+import { ReactSession } from "react-client-session";
 
 function LandingPage() {
+  const data = ["Users only can register either individual or group only.(individual player cannot be a member of any group)", "When you register please use your full registration number.(EX:EUSL/TC/IS/......)", "If you are a individual player use a unique name for your username.", "If you are a group please use a unique group name.", "Every task will be awarded by points.according to that points leaderboard will be updated every day.", "At the end of the 5th day user or team who gets the highest marks will be the winner.", "dont use any vpn to log into the game."];
+  const [insClicked, setInsClicked] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (ReactSession.get("un") == "undefined" || ReactSession.get("un") == ""){
+    if (ReactSession.get("un") == "undefined" || ReactSession.get("un") == "") {
       //navigate("/home");
-    }
-    else{
+    } else {
       navigate("/home");
     }
   });
-  
-  
+
   return (
     <Container>
+      {insClicked ? (
+        <div className="insContainer">
+          <div className="insBox">
+            <div className="instructions-rules">
+              <div className="title-content">
+                Instructions & Rules
+                <img src={Cross} alt="close" className="close-btn" onClick={() => setInsClicked(false)} />
+              </div>
+              <div className="points">
+                {data.map((data, key) => (
+                  <div className="point-container">
+                    <img src={Point} alt="points" key={key} />
+                    <div className="point">{data}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="background-image-container"></div>
       <div className="background-tint"></div>
       <div className="decoration-box"></div>
@@ -28,7 +52,9 @@ function LandingPage() {
         <div className="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, voluptatibus!</div>
       </div>
       <Login />
-      <div className="instructions">Read Instructions</div>
+      <div className="instructions" onClick={() => setInsClicked(true)}>
+        Read Instructions
+      </div>
     </Container>
   );
 }
@@ -41,6 +67,77 @@ const Container = styled.div`
   position: relative;
   overflow: hidden;
   z-index: 0;
+
+  .insContainer {
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    position: absolute;
+    backdrop-filter: blur(5px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .insBox {
+      width: 60%;
+      height: max-content;
+      max-height: 80%;
+      overflow: hidden;
+      padding: 20px;
+      overflow-y: auto;
+      background-color: var(--white);
+      box-shadow: 0 0 10px 0 var(--gray);
+
+      &::-webkit-scrollbar {
+        width: 5px;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: var(--theme1);
+      }
+
+      .instructions-rules {
+        .title-content {
+          font-size: 1.5rem;
+          font-weight: 600;
+          margin-bottom: 20px;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+
+          .close-btn {
+            width: 25px;
+            cursor: pointer;
+            transition: all 0.3s ease;
+
+            &:hover {
+              transform: scale(1.05);
+            }
+          }
+        }
+
+        .points {
+          margin-left: 0px;
+
+          .point-container {
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+            margin-left: 20px;
+
+            img {
+              width: 20px;
+            }
+
+            .point {
+              font-weight: 100;
+              margin-left: 20px;
+            }
+          }
+        }
+      }
+    }
+  }
 
   .background-image-container {
     width: 100%;
@@ -114,6 +211,12 @@ const Container = styled.div`
   }
 
   @media only screen and (max-width: 950px) {
+    .insContainer {
+      .insBox {
+        width: 80%;
+      }
+    }
+
     .title-container {
       width: 100%;
       top: 5%;
@@ -141,6 +244,26 @@ const Container = styled.div`
   }
 
   @media only screen and (max-width: 450px) {
+    .insContainer {
+      .insBox {
+        width: 90%;
+
+        .instructions-rules {
+          .title-content {
+            font-size: 1.2rem;
+          }
+
+          .points {
+            .point-container {
+              .point {
+                font-size: 0.8rem;
+              }
+            }
+          }
+        }
+      }
+    }
+
     .title-container {
       .title {
         font-size: 2rem;
